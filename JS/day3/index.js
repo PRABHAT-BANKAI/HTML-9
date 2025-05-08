3670760bd4a128f2e6a67bd26c2b3f91fbaac9f6
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-05-08 09:56:48 +0530

    variables

---
+++ JS/day3/index.js
@@ -0,0 +1,25 @@
+// what is variables understanding
+//  data types declaring and initialization variables
+
+// let , var, const // helps variable declaration 
+// "use strict" 
+ let userName = "batman" // value assign and initialization 
+
+ // javascript is dynamic language
+ let number = 12 // number
+ number = "superman" // string
+
+console.log(userName)
+console.log(number)
+console.log(5+5)
+
+let a = 2
+let b =5
+let result = a+b
+
+console.log(result)
+
+
+// camelCase  userName javaScript
+// user_name snake_case
+
