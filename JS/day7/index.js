5faaa07fef591c520ffa97dbbc8d7af6dc09fcf3
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-05-14 09:51:48 +0530

    ifelseday7

---
+++ JS/day7/index.js
@@ -0,0 +1,80 @@
+// if statement
+
+//syntax
+//if(condition){ // false //true
+
+//if condition is true then code will  execute inside this block
+
+//}
+
+// let number1 = 1;
+
+// let number2 = 15;
+
+// if (number1 > number2) {
+//   //false
+//   console.log("number1 is greater than number 2");
+// } else {
+//   console.log("number1 smaller than number2 ");
+// }
+
+let number = 10;
+
+console.log(number % 2);
+
+if (number % 2 == 0) {
+  console.log("number is even", number);
+} else {
+  console.log("number is odd", number);
+}
+
+let userName = "atman";
+
+if (userName == "batman") {
+  console.log("login successful");
+} else {
+  console.log("login failed");
+}
+
+const age = 11;
+
+if (age >= 18) {
+  console.log("eligible for driving licence");
+} else {
+  console.log("not elgible for driving licence");
+}
+
+let studentMarks = 66;
+
+if (studentMarks > 90) {
+  console.log("you will get  bike or iPhone");
+} else {
+  console.log("pitai");
+}
+
+let grade = 15;
+
+if (grade > 40) {
+  console.log("c");
+} else {
+  console.log("maybe he have good grade");
+}
+if (grade > 40) {
+  console.log("d");
+}
+
+/// ternary operator   conditon ? trueExecute : false Execute
+
+13 % 2 == 0 ? console.log("Even") : console.log("odd");
+
+let max = Infinity;
+let min = -Infinity;
+
+
+console.log(1+ +"1234")
+
+
+
+
+
+
