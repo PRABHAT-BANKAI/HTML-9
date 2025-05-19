509a27c77639af9ef098d73e72789760a85a9e74
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-05-19 09:58:29 +0530

    day9

---
+++ JS/day9/index.js
@@ -0,0 +1,35 @@
+// if else ladder Statement Choice
+
+// if(false){
+//   console.log("hello")
+// }else{
+//   console.log("world")
+// }
+
+// else if
+
+let light = "red"; // red green yellow  // top to bottom check krta hai conditions ko
+
+if (light === "red") {
+  console.log("stop");
+} else if (light === "red") {
+  console.log("slow down ,ready");
+} else if (light === "green") {
+  console.log("go");
+} else {
+  console.log("Invalid color ");
+}
+
+let budget = 2200;
+
+if (budget > 4000) {
+  console.log("taj restaurent");
+} else if (budget > 3000) {
+  console.log("helock restauraent");
+} else if (budget > 2000) {
+  console.log("domoinos");
+} else if (budget > 1000) {
+  console.log("dhaba");
+} else {
+  console.log("ghar betho ");
+}
