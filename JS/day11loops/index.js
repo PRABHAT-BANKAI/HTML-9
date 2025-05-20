d4bf49511150549e216c3615c63a4a248f253843
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-05-20 09:46:35 +0530

    for Loop

---
+++ JS/day11loops/index.js
@@ -0,0 +1,63 @@
+// For loop Some reasoning programming
+
+// let i = 10
+
+// for(initialization;condition; increment ,decrement ){
+
+//   // after condition is true then it will excute the code in block{}
+
+// }
+
+// for(let i=1;i<=6;i++){
+//   console.log(i)
+// }
+//i=7// condition 7<=6 false
+
+//1
+//2
+//3
+//4
+//5
+//6
+
+// for(let i=1;i<=20;i++){
+
+//   if(i%2==0){
+//     console.log(`even number is ${i} `)
+//   }else{
+//      console.log(`odd number is ${i} `)
+//   }
+
+// }
+
+// 100 to 1
+
+// for (let i = 100; i >= 1; i--) {
+//   console.log(i);
+// }
+
+// 12345 output
+
+
+// let string = ""
+
+// for(let i=1;i<=5;i++){
+//   // string = string + i // "" +1  "12345"
+//   string+= i // concatenate
+// }
+
+// console.log(string)
+
+// 12345678910
+let sum = 0
+for(let i=1;i<=10;i++){
+sum = sum +i
+}
+
+console.log(sum)
+
+
+// 5!  5*4*3*2*1
+
+
+
