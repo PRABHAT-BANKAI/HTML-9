11df4b52587200dc0eaae052717b4c283c274e50
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-08-14 09:48:32 +0530

    import export

---
+++ react/day4installation/exportimport/multi.js
@@ -0,0 +1,5 @@
+const multiple = (a, b) => {
+  return a * b;
+};
+
+export default multiple
---
+++ react/day4installation/exportimport/sum.js
@@ -0,0 +1,9 @@
+const sum = function (a, b) {
+  console.log(a + b);
+};
+
+const sub = function (a, b) {
+  console.log(a - b);
+};
+
+export { sum , sub };
