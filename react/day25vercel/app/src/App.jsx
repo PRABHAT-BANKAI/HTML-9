fcb39b396c6c98dff3f706d629934307c1db3940
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-10-16 09:50:51 +0530

    vercel3

--- react/day25vercel/app/src/App.jsx
+++ react/day25vercel/app/src/App.jsx
@@ -24,6 +24,7 @@ const App = () => {
 
       <div className="content">
         <Routes>
+          
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
