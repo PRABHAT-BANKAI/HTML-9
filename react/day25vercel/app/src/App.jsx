ff20619260c69e529216c9c8ff290e0addf8ee16
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-10-16 09:33:13 +0530

    vercel3

--- react/day25vercel/app/src/App.jsx
+++ react/day25vercel/app/src/App.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes, Link } from "react-router";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import './App.css'; // Import CSS
 
 const App = () => {
   return (
     <div>
-      <h1>hello world</h1>
-    </div>
-  )
-}
+      <nav className="navbar">
+        <ul className="nav-links">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+      </nav>
 
-export default App
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </div>
+  );
+};
+
+export default App;
